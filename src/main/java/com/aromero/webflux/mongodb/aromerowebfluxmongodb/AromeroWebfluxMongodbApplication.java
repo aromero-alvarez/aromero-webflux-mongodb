7cd7c951aaded7ec26720e6d0dc0bb8c433d679b
Abrahan Romero <aromero@lendingclub.com>
2018-12-12 14:43:26 -0800

    Init project.

---
+++ src/main/java/com/aromero/webflux/mongodb/aromerowebfluxmongodb/AromeroWebfluxMongodbApplication.java
@@ -0,0 +1,14 @@
+package com.aromero.webflux.mongodb.aromerowebfluxmongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AromeroWebfluxMongodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AromeroWebfluxMongodbApplication.class, args);
+	}
+
+}
+
